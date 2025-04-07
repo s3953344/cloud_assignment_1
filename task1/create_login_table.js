4015c9ff1601620e89d7151a24fef55c994d3be6
s3953344 <s3953344@student.rmit.edu.au>
2025-04-07 23:49:32 +1000

    initial commit

---
+++ task1/create_login_table.js
@@ -0,0 +1,30 @@
+import { CreateTableCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+
+export const main = async () => {
+  const command = new CreateTableCommand({
+    TableName: "EspressoDrinks",
+    // For more information about data types,
+    // see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes and
+    // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.LowLevelAPI.html#Programming.LowLevelAPI.DataTypeDescriptors
+    AttributeDefinitions: [
+      {
+        AttributeName: "DrinkName",
+        AttributeType: "S",
+      },
+    ],
+    KeySchema: [
+      {
+        AttributeName: "DrinkName",
+        KeyType: "HASH",
+      },
+    ],
+    BillingMode: "PAY_PER_REQUEST",
+  });
+
+  const response = await client.send(command);
+  console.log(response);
+  return response;
+};
+
