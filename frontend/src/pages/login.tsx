df605c9e9840bc18d9caad45739569547cb4c5d8
s3953344 <s3953344@student.rmit.edu.au>
2025-04-08 11:02:36 +1000

    login field validation messages

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,30 +1,51 @@
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import "./formValidation.css";
+
+// interface FormData {
+//   email: string;
+//   password: string;
+// }
+
+const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function LoginPage() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  // const [data, setData] = useState("");
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  }
 
   return (
     <div className="login-page d-flex flex-column">
       <h1 className="text-4xl font-bold mb-4">Login</h1>
-      <form>
+      <form onSubmit={handleSubmit((data) => onSubmit(data))}>
         <div className="mb-4 d-flex flex-column">
-          <label className="mb-2" htmlFor="username">
-            Username
+          <label className="mb-2" htmlFor="email">
+            Email
           </label>
           <input
+            {...register("email", {required: true, pattern: emailPattern})}
             type="text"
-            id="username"
-            placeholder="Enter your username"
+            id="email"
+            className="form-control"
+            placeholder="Enter your email"
           />
+          {errors.email && <span role="alert" className="error-message">Enter a valid email address</span>}
         </div>
         <div className="mb-4 d-flex flex-column">
           <label className="mb-2" htmlFor="password">
             Password
           </label>
           <input
+            {...register("password", {required: "Please enter your password"})}
             type="password"
             id="password"
+            className="form-control"
             placeholder="Enter your password"
           />
+          {errors.password && <span role="alert" className="error-message">{errors.password.message?.toString()}</span>}
         </div>
         <p className="mb-4">
           Don't have an account? Register <Link to="/register">here</Link>
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,3 +1,6 @@
+import "./formValidation.css";
+
+
 export default function RegisterPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
