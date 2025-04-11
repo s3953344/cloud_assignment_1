304069dd5ea5ece5d503db6b1aac7dc75474d45e
s3953344 <s3953344@student.rmit.edu.au>
2025-04-11 15:31:25 +1000

    query area html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useAuth, USER_KEY } from './context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import { FieldValues, useForm } from 'react-hook-form';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [queryResults, setQueryResults] = useState<any>(null);
   const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
+  const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
 
   useEffect(() => {
     if (!isAuthenticated) { navigate("/login") }
@@ -18,13 +24,45 @@ function App() {
     logout();
     navigate("/login");
   }
+
+  const handleQuery = (data: FieldValues) => {
+
+  }
   
 
   return (
+    <>
+    <NavBar handleLogout={handleLogout}></NavBar>
     <div className="home-page">
-      <NavBar handleLogout={handleLogout}></NavBar>
       <h1>Music Subscription</h1>
+
+      <div className="even-columns">
+        <div className="query-area">
+          <h2>Query</h2>
+          <form onSubmit={handleSubmit((data) => handleQuery(data))}>
+            <div className="query-field"><label htmlFor="artist">Artist</label><input type="text" /></div>
+            <div className="query-field"><label htmlFor="title">Title</label><input type="text" /></div>
+            <div className="query-field"><label htmlFor="album">Album</label><input type="text" /></div>
+            <div className="query-field"><label htmlFor="year">Year</label><input type="number" /></div>
+            <button type='submit' className='btn btn-info'>Query</button>
+          </form>
+          <div className="query-results">
+            {
+              queryResults !== null ?
+              queryResults
+              :
+              "No result is retrieved. Please query again"
+            }
+          </div>
+        </div>
+
+        <div className="subscription-area">
+          <h2>Subscriptions</h2>
+        </div>
+      </div>
     </div>
+
+    </>
   )
 }
 
